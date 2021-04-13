015373464207974c3e1c075c68012becad5538d1
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-04-13 14:57:57 +0200

    added canvas proportions, made it so that periods are stored in arrays and made the canvas extend to the size value of the array containing all the values

--- timelineMaker.js
+++ timelineMaker.js
@@ -1,5 +1,18 @@
 let barCounter = 1;
+let barArray = [];
+let lengthOfTheCanvas = [];
+ /*  add array for colors  */
 const _containerHoldingBars = document.getElementById('containerHoldingBarsID');
+
+document.getElementById('addBtnID1').addEventListener('click', function(){
+    let newBar1 = document.getElementById('bar1ID');
+    let barInputEnd1 = document.getElementById('endYear1');
+    let barInputStart1 = document.getElementById('startYear1'); 
+    let yearsInsideBar1 = barInputEnd1.value - barInputStart1.value;
+    lengthOfTheCanvas.push(yearsInsideBar1);
+    barArray.push(newBar1);
+    newBar1.style.display =  "none";
+})
 
 document.getElementById('newBarBtnID').addEventListener('click',function(){
     barCounter += 1;
@@ -28,6 +41,14 @@ document.getElementById('newBarBtnID').addEventListener('click',function(){
     newBarInputEnd.setAttribute('ID', `endYear${barCounter}`);
     newBarInputColor.setAttribute('ID', `chosenColor${barCounter}`);
     newBarInputBtn.setAttribute('ID', `addBtnID${barCounter}`);
+
+    newBarInputBtn.addEventListener('click', function(){
+        barArray.push(newBar);
+        let yearsInsideBar = newBarInputEnd.value - newBarInputStart.value;
+        lengthOfTheCanvas.push(yearsInsideBar);
+        newBar.style.display =  "none";
+
+    })
     newBarInputStartLabel.setAttribute('for', `barInputStart${barCounter}`);
     newBarInputEndLabel.setAttribute('for', `barInputEnd${barCounter}`);
     newBarInputColorLabel.setAttribute('for', `chosenColor${barCounter}`);
@@ -39,4 +60,23 @@ document.getElementById('newBarBtnID').addEventListener('click',function(){
     newBar.appendChild(newBarInputColor);
     newBar.appendChild(newBarInputBtn);
     _containerHoldingBars.appendChild(newBar);
+})
+
+document.getElementById('createCanvasBtnID').addEventListener('click', function(){
+    console.log(barArray);
+    console.log(lengthOfTheCanvas);
+
+    let canvas = document.getElementById('theTimelineCanvas');
+    let ctx = canvas.getContext('2d');
+    canvas.height = "150";  
+    let  sum = 0;
+    for(let i = 0; i < lengthOfTheCanvas.length; i++){
+        sum += lengthOfTheCanvas[i];
+    }
+    console.log(sum);
+    canvas.width = `${sum * 10}`; 
+
+    /* make a for loop out of this   */
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(0, 0, 50, 50);
 })
